f7f1eebb5c182ba9c51619ed81f84c66b66ef9b3
dashuaige <superboss01@163.com>
2021-06-01 19:31:40 +0800

    iii

---
+++ api/controllers/MpController.php
@@ -0,0 +1,49 @@
+<?php
+namespace api\controllers;
+
+
+use api\models\UserOrder;
+use api\models\ZbCoinBricks;
+use common\models\User;
+use yii\db\Exception;
+
+/**
+ * https://easywechat.com/docs/3.x/overview
+ * maxwen\easywechat 依赖 3.x 版本
+ * @package api\controllers
+ */
+class MpController extends BaseController
+{
+    public function authOptional()
+    {
+        return ['*'];
+    }
+
+    public function actionServer(){
+        /* @var $wechat \maxwen\easywechat\Wechat */
+        $wechat = \Yii::$app->wechat;
+        /* @var $server \EasyWeChat\Server\Guard */
+        $server = $wechat->server;
+
+        $server->setMessageHandler(function ($message) {
+            // $message->FromUserName // 用户的 openid
+            // $message->MsgType // 消息类型：event, text....
+            return "您好！欢迎关注我!";
+        });
+
+        $response = $server->serve();
+        return $response;
+    }
+
+    public function actionLogin()
+    {
+        /* @var $wechat \maxwen\easywechat\Wechat */
+        $wechat = \Yii::$app->wechat;
+
+        if (!$wechat->isAuthorized())
+            return $wechat->authorizeRequired();
+        $openid = $wechat->getUser()->getOpenId();
+        return $this->redirect('http://baidu.com?openid=' . $openid);
+    }
+
+}
